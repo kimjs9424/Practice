ef84cdfb234688962517e17e59e8c9f9c78bde47
kimjs <kimjs9424@naver.com>
2017-07-04 22:56:42 +0900

    최초

--- app/src/main/java/com/example/kimjs/practice/MainActivity.java
+++ app/src/main/java/com/example/kimjs/practice/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.kimjs.practice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn1 : {
+                Toast.makeText(this, "버튼 1 클릭됨", Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+        }
+
     }
 }
